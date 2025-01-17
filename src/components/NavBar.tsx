1971ca6068402ef68207d8ad907a86f871296334
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2025-01-17 09:52:59 -0500

    addd

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -34,19 +34,19 @@ const NavBar = () => {
       <ul className={!open ? styles.listaContainer : styles.showMenu}>
         <div className={styles.containerLogo}>
           <Image src={require('../../public/imagenes/logouipApp.png')} alt='logo' width={60} height={40} className='object-cover' />
-          <span className='text-sky-50 uppercase'>Hola 👋 {session?.user?.name}</span>
+          <span className='text-slate-900 uppercase'>Hola 👋 {session?.user?.name}</span>
 
         </div>
 
         <div className={styles.ulContainer}>
-          <Link href={'/torneos'} className='text-[18px] text-sky-50 hover:text-blue-900 hover:underline font-semibold transition duration-500'>Torneos</Link>
-          <Link href={'/ligas'} className='text-[18px] text-sky-50 hover:text-blue-900 hover:underline font-semibold transition duration-500'>Ligas</Link>
-          <Link href={'/equipos'} className='text-[18px] text-sky-50 hover:text-blue-900 hover:underline font-semibold transition duration-500'>Equipos</Link>
-          <Link href={'/mercado'} className='text-[18px] text-sky-50 hover:text-blue-900 hover:underline font-semibold transition duration-500'>Mercado</Link>
-          <Link href={'/noticias'} className='text-[18px] text-sky-50 hover:text-blue-900 hover:underline font-semibold transition duration-500'>Noticias</Link>
+          <Link href={'/torneos'} className='text-[18px] text-slate-900 hover:text-blue-900 hover:underline font-semibold transition duration-500'>Torneos</Link>
+          <Link href={'/ligas'} className='text-[18px] text-slate-900 hover:text-blue-900 hover:underline font-semibold transition duration-500'>Ligas</Link>
+          <Link href={'/equipos'} className='text-[18px] text-slate-900 hover:text-blue-900 hover:underline font-semibold transition duration-500'>Equipos</Link>
+          <Link href={'/mercado'} className='text-[18px] text-slate-900 hover:text-blue-900 hover:underline font-semibold transition duration-500'>Mercado</Link>
+          <Link href={'/noticias'} className='text-[18px] text-slate-900 hover:text-blue-900 hover:underline font-semibold transition duration-500'>Noticias</Link>
         </div>
 
-        <IconButton onClick={logoutAuth} className='w-10 h-10 text-sky-50 hover:bg-slate-900 rounded-full p-2 transition-colors duration-500'><Tooltip title='Cerrar sesión'><Logout style={{ color: '#f0f9ff' }} /></Tooltip></IconButton>
+        <IconButton onClick={logoutAuth} className='w-10 h-10  hover:bg-sky-500 rounded-full p-2 transition-colors duration-500'><Tooltip title='Cerrar sesión'><Logout style={{ color: 'black' }} /></Tooltip></IconButton>
 
 
       </ul>
