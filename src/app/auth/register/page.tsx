f011f80d572b5c48d7d2608de9ddcc89a26580a9
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2025-06-21 14:28:40 -0500

    add new message

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -2,16 +2,23 @@
 
 import { FormRegister } from "@/components/forms/FormRegister";
 import styles from '../login/login.module.css'
-import Image from "next/image";
 
 
 
 export default function Register() {
-  
+
   return (
-     <main className={styles.containerLogin}>
-         <FormRegister/>
-      <div className={styles.image}/>
+    <main className={styles.containerLogin}>
+      <div className="w-2/4 bg-yellow-300 p-4 mt-10">
+        <p className="w-auto">
+          La página está en mantenimiento, disculpen
+          las molestias que puede ocasionar,
+          estamos trabajando en activarla lo mas pronto posible
+
+        </p>
+      </div>
+      <FormRegister />
+      <div className={styles.image} />
 
 
     </main>
